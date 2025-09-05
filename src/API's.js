0b78efcb87b69380d96b2bd6b10cdeaedd0f40fa
NiharikaBayyarapu <160631088+NiharikaBayyarapu@users.noreply.github.com>
2025-09-05 18:18:02 +0530

    Update API's.js

--- src/API's.js
+++ src/API's.js
@@ -1,2 +1,2 @@
-export const registerAPI = "https://backend-jobportal-final-4.onrender.com/api/auth/register";
-export const loginAPI = "https://backend-jobportal-final-4.onrender.com/api/auth/login";
+export const registerAPI = "https://backend-jobportal-final-2.onrender.com/api/auth/register";
+export const loginAPI = "https://backend-jobportal-final-2.onrender.com/api/auth/login";
